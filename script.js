1530972de666ca5112404687f962ba5aa361ef32
Elivelton <eliveltonmn@outlook.com>
2022-07-13 16:56:54 -0300

    Requisito 3 correto

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function verifyLogin() {
+function verifyLogin(event) {
+  event.preventDefault()
   const email = document.getElementById('email').value;
   const senha = document.getElementById('senha').value;
   if (email === 'tryber@teste.com' && senha === '123456') {
     window.alert('Olá, Tryber!');
   } else {
     window.alert('Email ou senha inválidos.');
-    console.log('aa');
   }
 }
 
   const buttonLogin = document.getElementById('buttonLogin');
-  buttonLogin.addEventListener('click',verifyLogin) ;
+  buttonLogin.addEventListener('click',verifyLogin);
